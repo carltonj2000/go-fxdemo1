264a0a166bd7319d37d18402cd195580d9afd05f
Carlton Joseph <carlton.joseph@gmail.com>
2023-05-17 20:54:37 -0700

    initial commit

---
+++ README.md
@@ -0,0 +1,12 @@
+# Go Lang Fx Demo #1
+
+## Code History
+
+## Code Creation
+
+```bash
+mkdir go-fxdemo1
+cd go-fxdemo1/
+go mod init github.com/carltonj2000/go-fxdemo1
+go get go.uber.org/fx@latest
+```
---
+++ main.go
@@ -0,0 +1,59 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"io"
+	"net"
+	"net/http"
+	"os"
+
+	"go.uber.org/fx"
+)
+
+func main() {
+	fx.New(
+		fx.Provide(
+			NewHTTPServer,
+			NewServerMux,
+			NewEchoHandler,
+		),
+		fx.Invoke(func(*http.Server) {})).Run()
+}
+
+func NewHTTPServer(lc fx.Lifecycle, mux *http.ServeMux) *http.Server {
+	srv := &http.Server{Addr: ":8080", Handler: mux}
+	lc.Append(fx.Hook{
+		OnStart: func(ctx context.Context) error {
+			ln, err := net.Listen("tcp", srv.Addr)
+			if err != nil {
+				return err
+			}
+			fmt.Println("Starting HTTP Server at", srv.Addr)
+			go srv.Serve(ln)
+			return nil
+		},
+		OnStop: func(ctx context.Context) error {
+			return srv.Shutdown(ctx)
+		},
+	})
+	return srv
+}
+
+type EchoHandler struct{}
+
+func NewEchoHandler() *EchoHandler {
+	return &EchoHandler{}
+}
+
+func (*EchoHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	if _, err := io.Copy(w, r.Body); err != nil {
+		fmt.Fprintln(os.Stderr, "Failed to handel request:", err)
+	}
+}
+
+func NewServerMux(echo *EchoHandler) *http.ServeMux {
+	mux := http.NewServeMux()
+	mux.Handle("/echo", echo)
+	return mux
+}
